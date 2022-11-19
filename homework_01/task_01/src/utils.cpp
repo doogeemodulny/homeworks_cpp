6a8a1f61eb00c434b26ad7976a35bcb2ec822a62
Eduard Andreev <doogee.modulny@gmail.com>
2022-11-19 08:53:09 +0000

    wip

--- homework_01/task_01/src/utils.cpp
+++ homework_01/task_01/src/utils.cpp
@@ -1,39 +1,39 @@
 #include "utils.hpp"
-#include <stack>
+
 #include <iostream>
+#include <stack>
 
 std::vector<std::string> SplitString(const std::string& data) {
   std::vector<std::string> elems;
-  std::string elem="";
-  int flag=0;
-  for(char const &letter: data){
-    if(flag==0){
-      if(letter=='('){
-        flag=1;
-      }
-      else if(letter!=' ' and letter!='\t'){
+  std::string elem = "";
+  int flag = 0;
+  for (char const& letter : data) {
+    if (flag == 0) {
+      if (letter == '(') {
+        flag = 1;
+      } else if (letter != ' ' and letter != '\t') {
         elem.push_back(letter);
-      }
-      else if(letter==' ' or letter=='\t'){
-        if (!elem.empty()){
+      } else if (letter == ' ' or letter == '\t') {
+        if (!elem.empty()) {
           elems.push_back(elem);
-          elem.clear();}
+          elem.clear();
+        }
       };
     };
-    if(flag==1){
-      if(letter!=')'){
+    if (flag == 1) {
+      if (letter != ')') {
         elem.push_back(letter);
-      }
-      else if(letter==')'){
+      } else if (letter == ')') {
         elem.push_back(letter);
-        if (!elem.empty()){
+        if (!elem.empty()) {
           elems.push_back(elem);
-          elem.clear();}
-        flag=0;
+          elem.clear();
+        }
+        flag = 0;
       };
     };
   };
-  if (!elem.empty()){
+  if (!elem.empty()) {
     elems.push_back(elem);
     elem.clear();
   }
